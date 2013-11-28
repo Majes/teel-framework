7063df5a495ff13e064fca11e0058fc56e35df0f
Guillaume <gbeauvoir@majes.fr>
2013-11-28 16:09:50 +0100

    fix kpn paginator

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Majes\CoreBundle\MajesCoreBundle(),
             new Majes\MediaBundle\MajesMediaBundle(),
             new Majes\CmsBundle\MajesCmsBundle(),
